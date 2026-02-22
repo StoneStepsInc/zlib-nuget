92b415c586dfb20b6ad7aa83be6d7b6319e695f6
Stone Steps <support@stonesteps.ca>
2026-02-21 21:43:44 -0500

    Reworked sample project to aid automated testing
    * The `applib` project doesn't need `Custom (D)` and `Custom (R)`
      configurations, but MSBuild fails to figure out the mapping
      between the standard and these custom configurations, making
      it harder to build all platform/configuration artifacts. Using
      identical configuration names in both projects makes it possible
      to build all of them via MSBuild.

--- sample-zpipe/build-and-test.bat
+++ sample-zpipe/build-and-test.bat
@@ -5,6 +5,18 @@ setlocal
 set ARCH="x64","Win32"
 set CONFIG="Custom (D)","Custom (R)","Debug","Release"
 
-for %%p in (%ARCH%) do for %%c in (%CONFIG%) do msbuild /t:Rebuild /p:Configuration="%%~c";Platform="%%~p" sample-zpipe\zpipe.vcxproj
+rem delete all output directories, so if one configuration fails, we don't run stale binaries
+for %%p in (%ARCH%) do rmdir /S /Q "sample-zpipe\\%%~p" "sample-zpipe\\applib\\%%~p"
 
+rem IntDir is set in VS, but not in MSBuild, and will default to project name under applib (i.e. applib/applib), which is not in .gitignore
+for %%p in (%ARCH%) do for %%c in (%CONFIG%) do msbuild /t:Rebuild /p:Configuration="%%~c";Platform="%%~p";IntDir="%%~p\\%%~c\\" sample-zpipe\zpipe.vcxproj
+
+echo.
+
+rem test the original zpipe redirection
 for %%p in (%ARCH%) do for %%c in (%CONFIG%) do echo %%~c/%%~p Sample text | "sample-zpipe\%%~p\%%~c\zpipe" | "sample-zpipe\%%~p\%%~c\zpipe" -d
+
+echo.
+
+rem test calling the project-level static library
+for %%p in (%ARCH%) do for %%c in (%CONFIG%) do echo %%~c/%%~p Sample text | "sample-zpipe\%%~p\%%~c\zpipe" -t AAAAAAABBBBBBBCCCCCCC
