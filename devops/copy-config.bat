787891f0aeee1b083ebe850a84a789a7ee5cfedf
Stone Steps <support@stonesteps.ca>
2026-02-21 21:18:53 -0500

    Reworked artifact collection to avoid sub-directories

--- devops/copy-config.bat
+++ devops/copy-config.bat
@@ -3,8 +3,8 @@
 setlocal
 
 rem keep the library name as-is (in zLib 1.3.2 the static library is named zs.lib)
-set ZLIB_STATICLIB_COMPAT=zs
-set PKG_ZLIB_STATICLIB=zlibstatic
+set ZLIB_STATIC=zs
+set ZLIB_STATIC_COMPAT=zlibstatic
 
 set PLATFORM=%~1
 set BLD_CONFIG=%~2
@@ -16,9 +16,9 @@ if "%PKG_CONFIG%" == "Debug" (set SUFFIX=d) else (set SUFFIX=)
 
 mkdir "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%"
 
-copy /Y "install\%PLATFORM%\%BLD_CONFIG%\lib\%ZLIB_STATICLIB_COMPAT%%SUFFIX%.lib" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\"
-copy /Y "install\%PLATFORM%\%BLD_CONFIG%\lib\%ZLIB_STATICLIB_COMPAT%%SUFFIX%.lib" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\%PKG_ZLIB_STATICLIB%%SUFFIX%.lib"
+copy /Y "install\%PLATFORM%\%BLD_CONFIG%\lib\%ZLIB_STATIC%%SUFFIX%.lib" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\"
+copy /Y "install\%PLATFORM%\%BLD_CONFIG%\lib\%ZLIB_STATIC%%SUFFIX%.lib" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\%ZLIB_STATIC_COMPAT%%SUFFIX%.lib"
 
-if EXIST "build\%PLATFORM%\%BLD_CONFIG%\%ZLIB_STATICLIB_COMPAT%%SUFFIX%.pdb" (
-  copy /Y "build\%PLATFORM%\%BLD_CONFIG%\%ZLIB_STATICLIB_COMPAT%%SUFFIX%.pdb" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\"
+if EXIST "build\%PLATFORM%\%BLD_CONFIG%\%ZLIB_STATIC%%SUFFIX%.pdb" (
+  copy /Y "build\%PLATFORM%\%BLD_CONFIG%\%ZLIB_STATIC%%SUFFIX%.pdb" "..\nuget\build\native\lib\%PLATFORM%\%PKG_CONFIG%\"
 )
